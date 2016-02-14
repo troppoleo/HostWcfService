3dff296c7e20da2dbcd385f80f04151bfc95e364
troppoleo <troppoleo@gmail.com>
2016-02-14 11:00:29 +0100

    modifiche

--- WcfServiceLibraryRestJson/IISrvRestJson.cs
+++ WcfServiceLibraryRestJson/IISrvRestJson.cs
@@ -12,11 +12,11 @@ namespace WcfServiceLibraryRestJson
     [ServiceContract]
     public interface ISrvRestJson
     {
-        [OperationContract]
-        string GetData(int value);
+        //[OperationContract]
+        //string GetData(int value);
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
 
 
         //RequestFormat=WebMessageFormat.Json
@@ -24,8 +24,35 @@ namespace WcfServiceLibraryRestJson
         [WebGet(ResponseFormat=WebMessageFormat.Json) ]
         string GetEcho(string pEcho);
 
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        string GetEcho2(string pEcho);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        string GetEcho3(string pEcho);
+
+
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json,
+        //    UriTemplate = "/ChiamataJsonComplessa", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        [WebInvoke ( Method="POST", RequestFormat=WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string DoPost(ParemterForPost pParemterForPost);
+
         // TODO: Add your service operations here
     }
+
+
+    [DataContract]
+    public class ParemterForPost
+    {
+        [DataMember]
+        public string par1 { get; set; }
+        [DataMember]
+        public string par2 { get; set; }
+    }
+
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibraryRestJson.ContractType".
